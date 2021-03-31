3bc10870669b1bb29c28e83d91b6ae316273d3a7
JohnOGram <john@minns.io>
2021-03-31 21:08:52 +0100

    Simple flow of control demo with user input

---
+++ 04 Flow of Control/Flow.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+    int firstNumber;
+    int secondNumber;
+
+    cout << "Enter a number: ";
+    cin >> firstNumber;
+    cout << "You entered " << firstNumber << ". Enter another number: ";
+    cin >> secondNumber;
+
+    if (firstNumber < secondNumber)
+    {
+        cout << firstNumber << " is less than " << secondNumber;
+    }
+
+    if (firstNumber == secondNumber)
+    {
+        cout << firstNumber << " is equal to " << secondNumber;
+    }
+
+    if (firstNumber > secondNumber)
+    {
+        cout << firstNumber << " is greater than " << secondNumber;
+    }
+    
+}
